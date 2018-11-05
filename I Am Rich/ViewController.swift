43ba0b56e5f966c6fc6a1370cda00efafa4a324d
Mustafa Alsoffi <mustafaalsoffi@Mustafas-MacBook.local>
2018-11-06 07:12:32 +0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mustafa Alsoffi on 06/11/2018.
+//  Copyright © 2018 Mustafa Alsoffi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
